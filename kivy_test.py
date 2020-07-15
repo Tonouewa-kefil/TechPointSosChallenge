4bb5087ead6bab03557b7aebc0ea6abc84edb47f
SenseiWaffle <34588952+SenseiWaffle@users.noreply.github.com>
2020-07-15 13:12:12 -0700

    Update kivy_test.py

--- kivy_test.py
+++ kivy_test.py
@@ -1,14 +1,51 @@
-import kivy
-kivy.require('1.11.1')
-
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.config import Config
 
+# Set window size
+#   Pixel dimensions for a Samsung Galaxy S9 are 1440 x 2960,
+#   scaled window by a factor of 0.25 to get 360 x 740
+Config.set('graphics', 'width', '360')
+Config.set('graphics', 'height', '740')
 
-class MyApp(App):
+# Create both screens. Please note the root.manager.current: this is how
+# you can control the ScreenManager from kv. Each screen has by default a
+# property manager that gives you the instance of the ScreenManager used.
+Builder.load_string("""
+<MenuScreen>:
+    BoxLayout:
+        Button:
+            text: 'Goto settings'
+            on_press: root.manager.current = 'settings'
+        Button:
+            text: 'Quit'
+
+<SettingsScreen>:
+    BoxLayout:
+        Button:
+            text: 'My settings button'
+        Button:
+            text: 'Back to menu'
+            on_press: root.manager.current = 'menu'
+""")
+
+# Declare both screens
+class MenuScreen(Screen):
+    pass
+
+class SettingsScreen(Screen):
+    pass
+
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(SettingsScreen(name='settings'))
+
+class TestApp(App):
+
     def build(self):
-        return Label(text="kivy")
+        return sm
 
-
-if __name__ == "__main__":
-    MyApp().run()
+if __name__ == '__main__':
+    TestApp().run()
