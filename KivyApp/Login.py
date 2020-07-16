7195b817c729f212b3cb4a47c082dca209d9177b
SenseiWaffle <34588952+SenseiWaffle@users.noreply.github.com>
2020-07-16 13:24:43 -0700

    Update Login.py

--- KivyApp/Login.py
+++ KivyApp/Login.py
@@ -4,12 +4,23 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.config import Config
+
+# Set window size
+#   Pixel dimensions for a Samsung Galaxy S9 are 1440 x 2960,
+#   scaled window by a factor of 0.25 to get 360 x 740
+Config.set('graphics', 'width', '360')
+Config.set('graphics', 'height', '740')
+
+# Load in the style document named 'Transit.kv'
+Builder.load_file( 'Transit.kv' )
 
 # Username (TestInput)
 # Password (TestInput)
 # Sign in  (Button)
 # Create Account (Button)
-class Login(Widget):
+class Login(Screen):
     pass
 
 # Welcome (Label)
@@ -19,13 +30,17 @@ class Login(Widget):
 # Password (TestInput)
 # Confirm (TestInput)
 # Create! (button)
-class CreateAccount(Widget):
+class CreateAccount(Screen):
     pass
+
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(Login(name='Login'))
+sm.add_widget(CreateAccount(name='CreateAccount'))
 
 class TransitApp(App):
     def build(self):
-        return Login()
-
+        return sm
 
 if __name__ == '__main__':
     TransitApp().run()
