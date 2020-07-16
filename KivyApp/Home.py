3dfe511fad11eac939be42593e4289fb184f1543
SenseiWaffle <34588952+SenseiWaffle@users.noreply.github.com>
2020-07-16 14:00:24 -0700

    Delete Home.py

--- KivyApp/Home.py
+++
@@ -1,7 +0,0 @@
-import kivy
-from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
