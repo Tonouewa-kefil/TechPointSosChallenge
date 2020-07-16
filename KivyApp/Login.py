f2dab4f397cb55a102aa7bfda478b6227fea5956
SenseiWaffle <34588952+SenseiWaffle@users.noreply.github.com>
2020-07-16 13:26:44 -0700

    Update Login.py

--- KivyApp/Login.py
+++ KivyApp/Login.py
@@ -1,5 +1,6 @@
 import kivy
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
