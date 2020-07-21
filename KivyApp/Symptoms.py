42d30eafee8c4c1b413338d56c51ab7d4a66a7c7
bradcj05 <67477831+bradcj05@users.noreply.github.com>
2020-07-21 16:06:01 -0400

    Add files via upload

---
+++ KivyApp/Symptoms.py
@@ -0,0 +1,32 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
+#class MySym():
+        #go_back: ObjectProperty(None)
+        #sym1 = ObjectProperty(None)
+        #sym2 = ObjectProperty(None)
+        #sym3 = ObjectProperty(None)
+        #sym4 = ObjectProperty(None)
+        #sym5 = ObjectProperty(None)
+        #sym6 = ObjectProperty(None)
+        #submit = ObjectProperty(None)
+        #Report = "Your current Symptoms are"
+
+        #def btn(self):
+        #    can create
+
+class SymApp(App):
+
+    def build(self):
+
+
+
+        #return MySym()
+
+if __name__ == '__main__':
+    SymApp().run()
